a752faef27a2239aaae7c247952ce2b4defdb81a
nlrowe <nathan.rowe@abcfinancial.com>
2017-10-24 17:43:57 -0500

    Add a test for the generic render.

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -5,12 +5,20 @@ import App from './'
 jest.mock('../api/gitHub', () => (() => Promise.resolve({login: 'nlrowe'})))
 
 describe('<App />', () => {
-    describe('render with profile', () => {
+    describe('basic render', () => {
         const app = shallow(<App />)
 
         it('should have the container class', () => {
             expect(app.hasClass('container')).toBe(true)
         })
+
+        it('should not render the greeting message before the profile is fetched', () => {
+            expect(app.find('span').length).toEqual(0)
+        })
+    })
+
+    describe('render with profile', () => {
+        const app = shallow(<App />)
 
         it('should render the greeting message in a span to the login name', () => {
             app.update()
@@ -20,10 +28,6 @@ describe('<App />', () => {
 
     describe('render without profile', () => {
         const app = shallow(<App />)
-
-        it('should have the container class', () => {
-            expect(app.hasClass('container')).toBe(true)
-        })
 
         it('should render the greeting message in a span to unknown', () => {
             app.setState({profile: {}})
